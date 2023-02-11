a131b35102bd0943db925624bcc440e6961e21a6
Shohan88 <125019088+Shohan88@users.noreply.github.com>
2023-02-12 02:11:51 +0600

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,13 @@
+public class class 2-2-2043{
+public static int fact(int n)
+        {
+         if (n==0);
+         return 1;
+         else
+         return n * fact("n-1");
+        }
+public static void main(String[] args){
+    int a=fact("5");
+    System.out.print(a);
+        }
+        }
